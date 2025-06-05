2cf9a33eb47abcf4a605d8a1951b04d227725d38
sunny-yadav <sunny9405683@gmail.com>
2025-06-05 16:00:13 +0530

    feat(answer-api): created api for uploading and deleting ans and user reputation

--- src/store/Auth.ts
+++ src/store/Auth.ts
@@ -5,7 +5,7 @@ import { persist } from "zustand/middleware";
 import { AppwriteException, ID, Models } from "appwrite";
 import { account } from "@/models/client/config";
 
-interface userPrefereces {
+export interface userPrefereces {
   reputation: number;
 };
 
